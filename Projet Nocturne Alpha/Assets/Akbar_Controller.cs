2b80965c34f81d63f245771b06e375de94360ca6
Arthur Llorens <arthurllorens@hotmail.fr>
2015-11-18 13:57:18 +0100

    first

---
+++ Projet Nocturne Alpha/Assets/Akbar_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class Akbar_Controller : MonoBehaviour {
+
+	private int speed = 10;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");				
+		transform.Translate(Vector3.forward * moveVertical * speed * Time.deltaTime);
+		transform.Rotate (0.0f, moveHorizontal * speed / 2.5f, 0.0f);
+	}
+}
