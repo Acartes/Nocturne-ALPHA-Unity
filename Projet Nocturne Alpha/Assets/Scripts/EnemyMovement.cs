09184ce35e136078473d2c7b9cec175c1b7a10b3
littlebr0ther <victorcottineau@gmail.com>
2015-11-27 13:08:56 +0100

    pathfinding
    pathfinding

---
+++ Projet Nocturne Alpha/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+    bool playerInRange;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void Update ()
+    {
+        if (playerInRange)
+        {
+        nav.SetDestination(player.position);
+        }
+    }
+}
